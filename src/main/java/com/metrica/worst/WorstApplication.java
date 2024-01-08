f175f98b65d6d7afd3173826d3211dea19c22dbb
gonzalorg879 <120451738+gonzalorg8799@users.noreply.github.com>
2024-01-08 10:36:57 +0100

    Add files via upload

---
+++ src/main/java/com/metrica/worst/WorstApplication.java
@@ -0,0 +1,13 @@
+package com.metrica.worst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorstApplication.class, args);
+	}
+
+}
