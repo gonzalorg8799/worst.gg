cf97016a61d29d8ed8715219bd895a1f05802d78
gonzalorg879 <gonzalorg8799@gmail.com>
2024-01-09 16:30:40 +0100

    tests for invocador class

--- src/test/java/com/metrica/worst/entities/InvocadorTest.java
+++ src/test/java/com/metrica/worst/entities/InvocadorTest.java
@@ -2,8 +2,8 @@ package com.metrica.worst.entities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,7 +17,7 @@ public class InvocadorTest {
     @BeforeEach
     public void setUp() {
         // Configuración inicial para cada prueba
-        List<Partida> partidas = Arrays.asList(new Partida("2882930"));
+        List<Partida> partidas = Arrays.asList(new Partida("PartidaId"));
         List<Campeon> campeones = Arrays.asList(new Campeon("nombreCampeon", 50.0, "championId",new Maestria(7,5000)));
         List<Double> winrateCampeon = Arrays.asList(60.0, 70.0, 80.0);
 
@@ -46,10 +46,28 @@ public class InvocadorTest {
         assertEquals("NuevoTag", invocador.getTag());
     }
 
+    @Test
+    public void testGetPartidas() {
+        assertNotNull(invocador.getPartidas());
+        assertEquals(1, invocador.getPartidas().size()); // Verifica que haya una partida en la lista
+        assertEquals("PartidaId", invocador.getPartidas().get(0).getPartidaId()); // Ajusta según la estructura de tu Partida
+    }
+
     // Repite este patrón para los demás atributos y sus respectivos getters y setters
 
     @Test
     public void testNotNull() {
         assertNotNull(invocador);
+    }
+
+    @Test
+    public void testWinrateCampeon() {
+        assertNotNull(invocador.getWinrateCampeon());
+        assertEquals(3, invocador.getWinrateCampeon().size()); // Verifica que haya tres valores en la lista
+
+        // Verifica algunos valores específicos
+        assertTrue(invocador.getWinrateCampeon().contains(60.0));
+        assertTrue(invocador.getWinrateCampeon().contains(70.0));
+        assertTrue(invocador.getWinrateCampeon().contains(80.0));
     }
 }
