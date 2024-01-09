1ce9b096da8b68cf1e2d31617863eb3463d2e505
Raul <Raul@DESKTOP-T5U920N>
2024-01-09 12:25:23 +0100

    creating campeon class + atributes

---
+++ src/main/java/com/metrica/worst/entities/Campeon.java
@@ -0,0 +1,11 @@
+package com.metrica.worst.entities;
+
+public class Campeon {
+
+//	<<--ATRIBUTES-->>	
+	private String nombre;
+	private double winRate;
+	private String championId;
+	private Maestria maestria;
+	
+}
