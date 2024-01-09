afd9cb42d8dc672ecbdb5888e9b0730b5ddc5815
Raul <Raul@DESKTOP-T5U920N>
2024-01-09 12:26:13 +0100

    adding getters and setters

--- src/main/java/com/metrica/worst/entities/Campeon.java
+++ src/main/java/com/metrica/worst/entities/Campeon.java
@@ -15,5 +15,37 @@ public class Campeon {
 		this.championId = championId;
 		this.maestria = maestria;
 	}
-	
+
+//	<<--GETTERS & SETTERS-->>
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getWinRate() {
+		return winRate;
+	}
+
+	public void setWinRate(double winRate) {
+		this.winRate = winRate;
+	}
+
+	public String getChampionId() {
+		return championId;
+	}
+
+	public void setChampionId(String championId) {
+		this.championId = championId;
+	}
+
+	public Maestria getMaestria() {
+		return maestria;
+	}
+
+	public void setMaestria(Maestria maestria) {
+		this.maestria = maestria;
+	}
 }
