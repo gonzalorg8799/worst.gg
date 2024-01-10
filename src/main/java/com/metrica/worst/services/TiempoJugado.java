e78cbe3b762b8dbf0af406a70c3175f60a766632
gonzalorg879 <gonzalorg8799@gmail.com>
2024-01-10 12:47:17 +0100

    changes in pom.xml add dependencies

--- src/main/java/com/metrica/worst/services/TiempoJugado.java
+++ src/main/java/com/metrica/worst/services/TiempoJugado.java
@@ -1,5 +1,40 @@
 package com.metrica.worst.services;
 
-public class TiempoJugado {
+import org.springframework.stereotype.Service;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+
+@Service
+public class TiempoJugado {
+	private String puuid;
+	private String apiKey;
+	
+	public TiempoJugado(String puuid, String apiKey) {
+		this.puuid=puuid;
+		this.apiKey=apiKey;
+	}
+	public TiempoJugado() {
+		
+	}
+	public String get(String puuid, String apiKey) {
+		this.puuid=puuid;
+		this.apiKey=apiKey;
+		String url = "https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/"+puuid+"/ids?start=0&count=20&api_key="+apiKey;
+		
+		Client cliente = ClientBuilder.newClient();
+    	WebTarget servicio;
+    	
+    	servicio=cliente.target(url);
+    	String crudo= servicio.request(MediaType.APPLICATION_JSON).get(String.class);
+    	JsonArray jsonArray = new JsonParser().parse(crudo).getAsJsonArray();
+    	String partida = jsonArray.get(0).toString();
+    	return partida;
+	}
 }
