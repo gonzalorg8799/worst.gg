f7f0368e20fdabe3d4ae422ff59fcd61d2273b65
Raul <Raul@DESKTOP-T5U920N>
2024-01-10 09:38:44 +0100

    atributes and 'before each' setup

--- src/test/java/com/metrica/worst/entities/CampeonTest.java
+++ src/test/java/com/metrica/worst/entities/CampeonTest.java
@@ -1,5 +1,21 @@
 package com.metrica.worst.entities;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class CampeonTest {
 
+	private Campeon campeon;
+	private Maestria maestria;
+	private Maestria comparar;
+	private Maestria nuevaMaestria;
+	private Maestria nuevaMaestriaComparar;
+	
+	@BeforeEach
+	public void setUp() {
+		maestria = new Maestria(7, 147231);
+		comparar = new Maestria(7, 147231);
+		nuevaMaestria = new Maestria(5, 17000);
+		nuevaMaestriaComparar = new Maestria(5, 17000);
+		campeon = new Campeon("Aatrox", 47, "147", maestria);
+	}
 }
