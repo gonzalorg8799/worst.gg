c431261a3a847360b33f90bf97cca36c475d388b
gonzalorg879 <gonzalorg8799@gmail.com>
2024-01-15 11:47:01 +0100

    hotfix apikey

--- src/main/java/com/metrica/worst/entities/ApiKey.java
+++ src/main/java/com/metrica/worst/entities/ApiKey.java
@@ -1,7 +1,7 @@
 package com.metrica.worst.entities;
 
 public class ApiKey {
-	private static final String apikey = "oVWHuDfVM5znZXhjPwkneRoIExoUHiKzVJNpPpp50-WlC96dDNKvv0QBpkAaMtl0J3ZnAh7QGM3GGw";
+	private static final String apikey = "RGAPI-0a88635e-2e60-48c4-b2cb-dc877c111f8b";
 
 	public static String getApikey() {
 		return apikey;
