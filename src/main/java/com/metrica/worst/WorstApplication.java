8849683aed3ad6d559c36a9a8eb28f242e2a402a
Raul Valentin Radu <raulvalentinradu@gmail.com>
2024-01-15 10:20:46 +0100

    controller implementation

--- src/main/java/com/metrica/worst/WorstApplication.java
+++ src/main/java/com/metrica/worst/WorstApplication.java
@@ -3,15 +3,11 @@ package com.metrica.worst;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.metrica.worst.services.EstadisticasPartidaService;
-
 @SpringBootApplication
 public class WorstApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(WorstApplication.class, args);
-		EstadisticasPartidaService c1 = new EstadisticasPartidaService("3590", "Karkinos", "RGAPI-751483ca-8277-40ed-811f-d267cf6d5da9");
-		c1.estadisticasPartida();
+		SpringApplication.run(WorstApplication.class, args);
 	}
 	
 	
---
+++ src/main/java/com/metrica/worst/controller/EstadisticasPartidaController.java
@@ -0,0 +1,35 @@
+package com.metrica.worst.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.metrica.worst.services.EstadisticasPartidaService;
+
+@RestController
+@RequestMapping("worst/gameStats")
+public class EstadisticasPartidaController {
+	/* ATRIBUTES */
+	EstadisticasPartidaService estadisticasPartida;
+	ConseguirPuuidService conseguirPuuid;
+
+	@Autowired
+	public EstadisticasPartidaController(EstadisticasPartidaService estadisticasPartida) {
+		this.estadisticasPartida = estadisticasPartida;
+	}
+	
+	/* METHOD */
+	
+	@GetMapping("{gameName}/{tagLine}")
+	public String getSpells(@PathVariable String gameName, String tagLine) {
+		try {
+			conseguirPuuid = new ConseguirPuuidService(gameName, tagLine);
+			return estadisticasPartida.estadisticasPartida();
+		}
+		catch(RuntimeException e) {
+			return "No games played in the last 7 days";
+		}
+	}
+}
