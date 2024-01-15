1fa9707d80c9afcc348f1c11d16b95101604cf31
gonzalorg879 <gonzalorg8799@gmail.com>
2024-01-15 10:49:25 +0100

    created the apikey class

---
+++ src/main/java/com/metrica/worst/entities/ApiKey.java
@@ -0,0 +1,10 @@
+package com.metrica.worst.entities;
+
+public class ApiKey {
+	private final String apikey = "RGAPI-0a88635e-2e60-48c4-b2cb-dc877c111f8b";
+
+	public String getApikey() {
+		return apikey;
+	}
+	
+}
