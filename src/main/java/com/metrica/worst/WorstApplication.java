8460fe485de95f7978543ad92a1a2f6930c03189
Raul Valentin Radu <raulvalentinradu@gmail.com>
2024-01-15 10:26:05 +0100

    removing faulty dependency

--- src/main/java/com/metrica/worst/WorstApplication.java
+++ src/main/java/com/metrica/worst/WorstApplication.java
@@ -3,15 +3,11 @@ package com.metrica.worst;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.metrica.worst.services.EstadisticasPartidaService;
-
 @SpringBootApplication
 public class WorstApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(WorstApplication.class, args);
-		EstadisticasPartidaService c1 = new EstadisticasPartidaService("top g", "Master G", "RGAPI-26d88b7e-412b-44b5-8cfd-e40e44ef056a");
-		c1.estadisticasPartida();
+		SpringApplication.run(WorstApplication.class, args);
 	}
 	
 	
